edbd07fc669e3943bb202a7e418a3c395b2b576a
Krish Chatterjie <chatterjie.krish@gmail.com>
2021-12-30 20:11:28 +0530

    fix: bug with object references

--- src/utils/parseResponse.ts
+++ src/utils/parseResponse.ts
@@ -1,11 +1,11 @@
 import { monTimings, tueTimings, wedTimings, thuTimings, friTimings } from '../constants/timings'
 import { courses } from '../constants/courses'
 
-let monSlots: any[] = []
-let tueSlots: any[] = []
-let wedSlots: any[] = []
-let thuSlots: any[] = []
-let friSlots: any[] = []
+const monSlots: any[] = []
+const tueSlots: any[] = []
+const wedSlots: any[] = []
+const thuSlots: any[] = []
+const friSlots: any[] = []
 
 export const parseAndUpload = (arr: any[]): any => {
   parseText(arr)
@@ -14,11 +14,11 @@ export const parseAndUpload = (arr: any[]): any => {
   addDetails(wedSlots, wedTimings)
   addDetails(thuSlots, thuTimings)
   addDetails(friSlots, friTimings)
-  monSlots = parseObject(monSlots, monTimings)
-  tueSlots = parseObject(tueSlots, tueTimings)
-  wedSlots = parseObject(wedSlots, wedTimings)
-  thuSlots = parseObject(thuSlots, thuTimings)
-  friSlots = parseObject(friSlots, friTimings)
+  parseObject(monSlots, monTimings)
+  parseObject(tueSlots, tueTimings)
+  parseObject(wedSlots, wedTimings)
+  parseObject(thuSlots, thuTimings)
+  parseObject(friSlots, friTimings)
   return [monSlots, tueSlots, wedSlots, thuSlots, friSlots]
 }
 
@@ -29,11 +29,11 @@ const parseText = (arr: any[]): any => {
     const slot = el.Slot
     if (!filledSlots.includes(slot)) {
       filledSlots.push(slot)
-      if (Object.prototype.hasOwnProperty.call(monTimings, slot)) monSlots.push(el)
-      if (Object.prototype.hasOwnProperty.call(tueTimings, slot)) tueSlots.push(el)
-      if (Object.prototype.hasOwnProperty.call(wedTimings, slot)) wedSlots.push(el)
-      if (Object.prototype.hasOwnProperty.call(thuTimings, slot)) thuSlots.push(el)
-      if (Object.prototype.hasOwnProperty.call(friTimings, slot)) friSlots.push(el)
+      if (Object.prototype.hasOwnProperty.call(monTimings, slot)) monSlots.push({ ...el })
+      if (Object.prototype.hasOwnProperty.call(tueTimings, slot)) tueSlots.push({ ...el })
+      if (Object.prototype.hasOwnProperty.call(wedTimings, slot)) wedSlots.push({ ...el })
+      if (Object.prototype.hasOwnProperty.call(thuTimings, slot)) thuSlots.push({ ...el })
+      if (Object.prototype.hasOwnProperty.call(friTimings, slot)) friSlots.push({ ...el })
     }
   })
   // console.log('Monday', monSlots)
@@ -43,11 +43,11 @@ const parseText = (arr: any[]): any => {
   // console.log('Friday', friSlots)
 }
 
-const changeTimezone = (date: Date, ianatz: string): Date => {
-  const invdate = new Date(date.toLocaleString('en-US', { timeZone: ianatz }))
-  const diff = date.getTime() - invdate.getTime()
-  return new Date(date.getTime() - diff)
-}
+// const changeTimezone = (date: Date, ianatz: string): Date => {
+//   const invdate = new Date(date.toLocaleString('en-US', { timeZone: ianatz }))
+//   const diff = date.getTime() - invdate.getTime()
+//   return new Date(date.getTime() - diff)
+// }
 
 const addMinutes = (date: Date, minutes: number): Date => new Date(date.getTime() + minutes * 60000)
 
@@ -56,12 +56,12 @@ const nullCheck = (x: any): any => (x == null ? '' : x)
 const addDetails = (daySlots: any[], timings: {[slot: string]: string}): void => {
   daySlots.forEach((el) => {
     // calculating timings
-    let startTime = new Date('April 1 2021 ' + timings[el.Slot] + ':00+05:30')
+    const startTime = new Date('April 1 2021 ' + timings[el.Slot] + ':00+05:30')
     let endTime: Date
     if (el.Course_type === 'Lab') endTime = addMinutes(startTime, 100)
     else endTime = addMinutes(startTime, 50)
-    startTime = changeTimezone(startTime, Intl.DateTimeFormat().resolvedOptions().timeZone)
-    endTime = changeTimezone(endTime, Intl.DateTimeFormat().resolvedOptions().timeZone)
+    // startTime = changeTimezone(startTime, Intl.DateTimeFormat().resolvedOptions().timeZone)
+    // endTime = changeTimezone(endTime, Intl.DateTimeFormat().resolvedOptions().timeZone)
 
     // calculating course name
     let courseName: string
@@ -73,37 +73,17 @@ const addDetails = (daySlots: any[], timings: {[slot: string]: string}): void =>
   })
 }
 
-const parseObject = (daySlots: any[], timings: {[slot: string]: string}): any[] => {
-  const newSlots: any[] = []
-
+const parseObject = (daySlots: any[], timings: {[slot: string]: string}): void => {
   daySlots.forEach((el) => {
     // reparsing object properly
-    const newEl = {
-      slot: '',
-      courseCode: '',
-      courseType: '',
-      courseName: '',
-      location: '',
-      startTime: new Date(),
-      endTime: new Date()
-    }
-    newEl.slot = nullCheck(el.Slot)
-    newEl.courseCode = nullCheck(el.Course_Name)
-    newEl.courseType = nullCheck(el.Course_type)
-    newEl.location = nullCheck(el.Venue)
-    newEl.courseName = el.courseName
-    newEl.startTime = el.startTime
-    newEl.endTime = el.endTime
-
-    newSlots.push(newEl)
-
-    // WHY DOESNT THIS JUST WORK
-    // delete el.Slot
-    // delete el.Course_Name
-    // delete el.Course_type
-    // delete el.Venue
+    el.slot = nullCheck(el.Slot)
+    el.courseCode = nullCheck(el.Course_Name)
+    el.courseType = nullCheck(el.Course_type)
+    el.location = nullCheck(el.Venue)
+    delete el.Slot
+    delete el.Course_Name
+    delete el.Course_type
+    delete el.Venue
   })
-
-  newSlots.sort((a, b) => (timings[a.Slot] > timings[b.Slot] ? -1 : 1))
-  return newSlots
+  daySlots.sort((a, b) => (timings[a.slot] > timings[b.slot] ? 1 : -1))
 }
