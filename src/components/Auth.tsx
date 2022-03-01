9f52bfef18fb5e134e56619ca8b3891885d2fa8d
Krish Chatterjie <chatterjie.krish@gmail.com>
2022-03-01 19:27:30 +0530

    feat: apple login

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import { AppContext } from './../Context'
-import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
-// import { FaApple } from 'react-icons/fa'
+import { getAuth, GoogleAuthProvider, OAuthProvider, getRedirectResult, signInWithRedirect } from 'firebase/auth'
+import { FaApple } from 'react-icons/fa'
 import { FcGoogle } from 'react-icons/fc'
 import '../styles/Auth.css'
 
@@ -10,12 +10,17 @@ const Auth: React.FC = () => {
   const [, setUser] = userState
 
   const auth = getAuth()
+  const appleProvider = new OAuthProvider('apple.com')
   const googleProvider = new GoogleAuthProvider()
   const logIn = (auth: any, provider: any): void => {
-    signInWithPopup(auth, provider)
+    void signInWithRedirect(auth, provider)
+    getRedirectResult(auth)
       .then((result) => {
-        const user = result.user
-        setUser(user.uid)
+        if (result !== null) {
+          const user = result.user
+          setUser(user.uid)
+          console.log(user)
+        }
       }).catch((error) => {
         // Handle Errors here.
         const errorCode = error.code
@@ -25,16 +30,15 @@ const Auth: React.FC = () => {
         console.log(errorCode, errorMessage, email)
       })
   }
-  // TODO: Apple sign in
   return (
     <div className='auth'>
       <h1>Welcome to VIT<span>TY</span></h1>
       <div className='google-sign-in'>
         <button onClick={() => logIn(auth, googleProvider)}> <FcGoogle /> Sign in with Google</button>
       </div>
-      {/* <div className='apple-sign-in'>
-        <button onClick={() => logIn(auth, googleProvider)}> <FaApple /> Sign in with Apple</button>
-      </div> */}
+      <div className='apple-sign-in'>
+        <button onClick={() => logIn(auth, appleProvider)}> <FaApple /> Sign in with Apple</button>
+      </div>
     </div>
   )
 }
