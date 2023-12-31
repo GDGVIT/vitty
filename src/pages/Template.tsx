c52f3b5b226ca0346c3b6feddf52b7933409a42d
Rupaak Srinivas <Rupaaksrinivas8@gmail.com>
2023-12-31 20:32:43 +0530

    style: added background image to template

--- src/pages/Template.tsx
+++ src/pages/Template.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
-import Navbar from '../components/Nav';
+import React from "react";
+import Navbar from "../components/Nav";
+import EllipseTR from "../assets/ellipse_tr.png";
+import EllipseBL from "../assets/ellipse_bl.png";
 
 interface TemplateProps {
   children: React.ReactNode;
@@ -8,10 +10,22 @@ interface TemplateProps {
 const Template: React.FC<TemplateProps> = ({ children }) => {
   return (
     <>
-    <Navbar/>
-    <div className="h-auto w-auto mt-[64px]">
-      {children}
-    </div>
+      <div className="fixed overflow-hidden opacity-50 ellipse ellipse-tr  right-0">
+        <img
+          className="w-72 overflow-hidden md:w-[35vw]"
+          src={EllipseTR}
+          alt="Vitty"
+        />
+      </div>
+      <div className="fixed overflow-hidden opacity-50 ellipse ellipse-bl bottom-0 -left-8">
+        <img
+          className="w-72 overflow-hidden md:w-[35vw]"
+          src={EllipseBL}
+          alt="Vitty"
+        />
+      </div>
+      <Navbar />
+      <div className="h-auto w-auto mt-[64px]">{children}</div>
     </>
   );
 };
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,29 +1,11 @@
-import EllipseTR from "../assets/ellipse_tr.png";
-import EllipseBL from "../assets/ellipse_bl.png";
 import HomeCarousal from "../components/HomeCarousal";
 import Auth from "../components/Auth";
 
 export default function Login() {
-
-    return (
-        <div className="mt-8 pb-8 flex flex-col items-center justify-around md:flex-row">
-
-            <div className="fixed overflow-hidden opacity-50 ellipse ellipse-tr  right-0">
-                <img
-                    className="w-72 overflow-hidden md:w-[35vw]"
-                    src={EllipseTR}
-                    alt="Vitty"
-                />
-            </div>
-            <div className="fixed overflow-hidden opacity-50 ellipse ellipse-bl bottom-0 -left-8">
-                <img
-                    className="w-72 overflow-hidden md:w-[35vw]"
-                    src={EllipseBL}
-                    alt="Vitty"
-                />
-            </div>
-            <HomeCarousal />
-            <Auth />
-        </div>
-    );
+  return (
+    <div className="mt-8 pb-8 flex flex-col items-center justify-around md:flex-row">
+      <HomeCarousal />
+      <Auth />
+    </div>
+  );
 }
