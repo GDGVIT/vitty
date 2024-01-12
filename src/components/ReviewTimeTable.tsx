8bee4076cf22e6e9a6e9e6fb238654700bdb5dce
Rupaak Srinivas <Rupaaksrinivas8@gmail.com>
2024-01-12 12:35:38 +0530

    feat: sorted timetable

--- src/components/ReviewTimeTable.tsx
+++ src/components/ReviewTimeTable.tsx
@@ -30,11 +30,25 @@ export default function ReviewTimeTable() {
     console.log(day, "day");
     console.log(timetable, "timetable");
     const sortedClasses = classes?.sort((a, b) => {
-      const aTime = a.start_time;
-      const bTime = b.start_time;
+      const aTime = convertTo24HourFormat(a.start_time);
+      const bTime = convertTo24HourFormat(b.start_time);
       return aTime.localeCompare(bTime);
     });
+
     setSortedClasses(sortedClasses || null);
+
+    function convertTo24HourFormat(time: string): string {
+      const [hour, minute, period] = time.split(/:| /);
+      let hour24 = parseInt(hour, 10);
+
+      if (period === "PM" && hour24 < 12) {
+        hour24 += 12;
+      } else if (period === "AM" && hour24 === 12) {
+        hour24 = 0;
+      }
+
+      return `${hour24.toString().padStart(2, "0")}:${minute}`;
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [timetable, day]);
 
